4444ddddaa3335667104dd0621731b9ef02f337b
ShashankShekhar696 <ShashankShekhar.Shukla@cognizant.com>
2025-12-29 17:29:37 +0530

    Redirection29-12-25ByShashank

---
+++ Backend/IAction.NET/IActionEX/IActionEX/Properties/AboutController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IActionEX.Properties
+{
+    public class AboutController : Controller
+    {
+        [Route("About-Us")]
+        public IActionResult Indexfn()
+        {
+            return Content("THIS PAGE IS ABOUT OUR ORGANISATION");
+        }
+    }
+}
