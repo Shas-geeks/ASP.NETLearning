38807a595602853ca037bbb1888c391f23a9256a
ShashankShekhar696 <ShashankShekhar.Shukla@cognizant.com>
2026-01-05 14:19:45 +0530

    ChangeInHomeControllerAndCreatedModelClass

--- Backend/IAction.NET/IActionEX/IActionEX/Properties/HomeController.cs
+++ Backend/IAction.NET/IActionEX/IActionEX/Properties/HomeController.cs
@@ -1,34 +1,43 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
-
+using IActionEX.Models;
 namespace IActionEX.Properties
 {
     public class HomeController : Controller
     {
         [Route("book")]
-        public IActionResult Index()
+        public IActionResult Index(Book book,bool? isLoggedIn,int? bookID)
         {
             // If Book id is not supplied in URL 
-            if(Convert.ToBoolean(Request.Query.ContainsKey("bookID"))==false)
-            {
+            //if(Convert.ToBoolean(Request.Query.ContainsKey("bookID"))==false)
+            //{
                 
+            //    return BadRequest("Book ID Not Supplied ");
+            //}
+            if(bookID.HasValue==false)
+            {
                 return BadRequest("Book ID Not Supplied ");
             }
             // IF Book ID is supplied with empty 
+            if (string.IsNullOrEmpty(Convert.ToString(bookID)))
+            {
+                return BadRequest("Book Id Can't Be Empty ");
+            }
             if(string.IsNullOrEmpty(Request.Query["bookID"]))
             {
                 return BadRequest("Book Id Can't Be Empty ");
             }
             // Book ID should be 1 - 1000 in between 
-            int bookID = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookID"]);
-            if (bookID < 0 || bookID > 1000)
+            //int bookID = Convert.ToInt32(ControllerContext.HttpContext.Request.Query["bookID"]);
+            //int bookID = (int)Convert.ToInt64(book.BookID);
+            if (bookID < 0 || bookID > 1000)  
                 
                 return BadRequest("BookID must be within (0-1000) ");
             //else if(bookID >= 0 || bookID <= 1000)
             //{
             //    return Content("You Can Acces The Databases");
             //}
-            if (!Convert.ToBoolean(Request.Query["isLoggedIn"]))
+            if (isLoggedIn==false)
             {
                 Response.StatusCode = 401;
                 return Unauthorized("User Must Be Authenticated Daa !!");
